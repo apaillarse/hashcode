5b93873eec33f56a45821e95390f8c434c60dcbc
Aurelien PAILLARSE <a.paillarse@kayrros.com>
2020-02-20 19:46:21 +0100

    added scores

--- model/library.py
+++ model/library.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Library:
     def __init__(self, id, nb_days_to_signup, nb_books_per_day):
         self.id = id
@@ -10,6 +13,24 @@ class Library:
 
     def __str__(self):
         return f'Library #{self.id} with nb days to sign up {self.nb_days_to_signup} and nb books per day {self.nb_books_per_day}'
+
+    def get_nb_books(self):
+        self.nb_books = len(self.books)
+
+    def score_signup(self):
+        """Only account for time to signup
+
+        """
+        self.books_score = np.sum([v.score for v in self.books.values()])
+        return self.books_score / self.nb_days_to_signup
+
+    def score_signup_scan(self):
+        """Signup + scan
+        """
+        signup = self.score_signup()
+        self.get_nb_books()
+        scan = self.books_score * self.nb_books_per_day / self.nb_books
+        return signup + scan
 
 
 class LibraryBuilder:
